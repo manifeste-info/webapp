0d709ee9769cbd17ecb9eca2260ba516d6b0b50f
eze-kiel <ezekiel@fastmail.com>
2022-04-28 18:52:10 +0200

    fix: update db host

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	var host, port, user, pass, name string
 	var mgAPIKey string
 	fs := flag.NewFlagSetWithEnvPrefix(os.Args[0], "MANIFESTE", 0)
-	fs.StringVar(&host, "db-host", "database", "Database host")
+	fs.StringVar(&host, "db-host", "postgres", "Database host")
 	fs.StringVar(&port, "db-port", "5432", "Database port")
 	fs.StringVar(&user, "db-user", os.Getenv("POSTGRES_USER"), "Database user")
 	fs.StringVar(&pass, "db-pass", os.Getenv("POSTGRES_PASSWORD"), "Database pass")
